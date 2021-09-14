ed88567ad0ab3c1e7fb58c9f15d964fde7c43aa2
Jcoder2022 <JCoder2022@gmail.com>
2021-09-14 11:58:51 +0100

    while Loop

--- main.py
+++ main.py
@@ -41,17 +41,36 @@ while i <= 5:
     i += 1
 print('DONE')
 
+
+#  Guess Game
+
+secret_number = 4
+guess_limit = 3
+guess_count = 1
+while guess_count <= guess_limit:
+    guess_count += 1
+    guess = int(input("Guess: "))
+    if guess == secret_number:
+        print(f"You won!!! {secret_number} is secret number")
+        break # to terminate the loop
+else: # when all iterations are done , similar to if else
+    print("You lose, Try next time")
+
+
 #  Guess Game
 
 secret_number = 4
 number_of_iteration = 3
 i = 1
 while i <= number_of_iteration:
+    i += 1
     guess = int(input("Guess: "))
     if guess == secret_number:
         print(f"You won!!! {secret_number} is secret number")
-    i += 1
+        break # to terminate the loop
     if i >= 4:
         response = input("You want to try again Y/N: ")
         if response.lower() == 'y':
             i = 0
+
+# Car Game
